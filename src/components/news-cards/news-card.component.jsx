fbaea8b45e8ffd06770fc24f42591fa62a6b92c8
BrunoUA96 <ostapklysa@gmail.com>
2023-09-05 14:18:05 +0100

    arrangement of elements

--- src/components/news-cards/news-card.component.jsx
+++ src/components/news-cards/news-card.component.jsx
@@ -1,22 +1,25 @@
-export const NewsCart = ({ isBanner = false }) => {
+export const NewsCart = () => {
    return (
       <>
-         <div className={`flex gap-3 cursor-pointer group ${isBanner ? 'flex-col mb-8' : 'mb-5'}`}>
+         <div className="grid grid-cols-12 gap-10 cursor-pointer group mb-5">
             {/* Image */}
-            <div className="flex-shrink-0">
+            <div className="flex-shrink-0 col-span-3 overflow-hidden">
                <img
-                  className={`object-cover ${isBanner ? 'w-full max-h-52' : 'w-24 h-24'}`}
+                  className="object-cover w-full max-h-52 group-hover:scale-125 ease-in-out duration-300"
                   src={
                      'https://i.guim.co.uk/img/media/55b58f9514a6ccb5a57d59d04151af12864acf69/0_374_5616_3370/master/5616.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=416add2213cc99b0e4b11206ea66407b'
                   }
                   alt="News image"
                />
             </div>
-            {/* Title */}
-            <div className="flex flex-col justify-between">
-               <h5 className="text-2xl text-gray-700 group-hover:text-gray-800 ease-in-out duration-300 font-bold tracking-wider font-title mb-3">
+
+            <div className="flex flex-col justify-between col-span-9">
+               <div className="text-sm text-gray-400 font-light group-hover:text-yellow-600 ease-in-out duration-300">
+                  <span>2h ago</span>
+               </div>
+               <h5 className="text-xl text-gray-700 group-hover:text-gray-800 ease-in-out duration-300 font-bold tracking-wider font-title mb-3">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo omnis eligendi
-                  obcaecati, recusandae dolores exercitationem sapiente ipsum minus.
+                  obcaecati.
                </h5>
                <p className="line-clamp-3 text-gray-500 mb-3">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga in laborum libero
@@ -24,7 +27,6 @@ export const NewsCart = ({ isBanner = false }) => {
                   sunt corporis perspiciatis voluptatem aliquam excepturi iste?
                </p>
                <div className="text-sm text-gray-400 font-light group-hover:text-yellow-600 ease-in-out duration-300">
-                  <span>2h ago</span>
                   <span>by Isabella Kwai</span>
                </div>
             </div>
--- src/pages/main.page.jsx
+++ src/pages/main.page.jsx
@@ -1,15 +1,23 @@
 import { Categories } from '../components/categories.component';
 import { Header } from '../components/header.component';
-import { NewsCart } from '../components/news/news-cart.component';
+import { BannerCard } from '../components/news-cards/banner-card.component';
+import { NewsCart } from '../components/news-cards/news-card.component';
 
 export const Main = () => {
    return (
-      <div className="container px-4 mt-4 mx-auto font-primary">
+      <div className="container max-w-screen-lg px-4 mt-4 mx-auto font-primary">
          <Header />
          <Categories />
 
-         <div className="grid grid-cols-2">
-            <NewsCart isBanner />
+         <div className="grid grid-cols-12 gap-10 mb-12 border-b pb-5">
+            <div className="col-span-8">
+               <BannerCard isPrincipal />
+            </div>
+
+            <div className="flex flex-col gap-3 col-span-4">
+               <BannerCard />
+               <BannerCard />
+            </div>
          </div>
 
          <NewsCart />
