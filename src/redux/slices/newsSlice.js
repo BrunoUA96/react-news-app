8192a1a0efd5565cd1b1057e2593ae409015f832
BrunoUA96 <ostapklysa@gmail.com>
2023-09-27 23:26:10 +0100

    create createAsyncThunk news

--- src/redux/slices/newsSlice.js
+++ src/redux/slices/newsSlice.js
@@ -1,7 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchNews = createAsyncThunk('news/fetchNewsStatus', async () => {
+   const { data } = await axios.get(
+      `${import.meta.env.VITE_API_URL}latest-news?apiKey=${import.meta.env.VITE_API_KEY}`,
+   );
+
+   return data;
+});
 
 const initialState = {
    news: [],
+   status: 'loading',
 };
 
 export const newsSlice = createSlice({
@@ -14,6 +24,20 @@ export const newsSlice = createSlice({
       removeAllNews: (state) => {
          state.news = [];
       },
+   },
+   extraReducers: (builder) => {
+      builder.addCase(fetchNews.pending, (state) => {
+         state.status = 'loading';
+         console.log('fetchNews is pending');
+      }),
+         builder.addCase(fetchNews.fulfilled, (state, action) => {
+            state.news = action.payload.news;
+            state.status = 'loaded';
+            console.log('fetchNews is loaded');
+         }),
+         builder.addCase(fetchNews.rejected, (state) => {
+            console.log('fetchNews is ERROR');
+         });
    },
 });
 
