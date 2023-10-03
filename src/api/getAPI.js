2878b4be70c5c97380488e5fd16a3054e2ce939f
BrunoUA96 <ostapklysa@gmail.com>
2023-10-03 17:35:28 +0100

    change folder

---
+++ src/api/getAPI.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const baseURL = import.meta.env.VITE_API_URL;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+export const getAPI = async (url, params = {}) => {
+   const response = await axios.get(`${baseURL}${url}`, {
+      params: { apiKey, ...params },
+   });
+
+   return response;
+};
