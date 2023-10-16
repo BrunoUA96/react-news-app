72e20ba6de463e1d661b830d63fa8ab2034bc2d5
BrunoUA96 <ostapklysa@gmail.com>
2023-10-16 18:04:31 +0100

    remove props

--- src/components/Layout.component.tsx
+++ src/components/Layout.component.tsx
@@ -1,19 +1,14 @@
-import { FC, PropsWithChildren, useState } from 'react';
+import { FC, PropsWithChildren } from 'react';
 
 import { Categories, Header } from '.';
 
 interface LayoutProps {}
 
 export const Layout: FC<PropsWithChildren<LayoutProps>> = ({ children }) => {
-  const [activeCategory, setActiveCategory] = useState('All');
-
   return (
     <div className="container max-w-screen-lg px-4 mt-4 mx-auto font-primary overflow-hidden">
       <Header />
-      <Categories
-        activeCategory={activeCategory}
-        setActiveCategory={setActiveCategory}
-      />
+      <Categories />
       {children}
     </div>
   );
