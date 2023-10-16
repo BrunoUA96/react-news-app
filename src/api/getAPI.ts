d66fe18def774008b2312beacb63eceef2ff6d31
BrunoUA96 <ostapklysa@gmail.com>
2023-10-16 13:12:55 +0100

    create rtk query api

--- src/api/getAPI.ts
+++ src/api/getAPI.ts
@@ -1,3 +1,4 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import axios from 'axios';
 
 const baseURL = import.meta.env.VITE_API_URL;
@@ -10,3 +11,52 @@ export const getAPI = async (url: string, params = {}) => {
 
   return response;
 };
+
+type newsDTO = {
+  news: newsInterface[];
+};
+
+interface newsInterface {
+  id: string;
+  image: string;
+  title: string;
+  description: string;
+  author: string;
+}
+
+type CategoriesDTO = {
+  categories: string[];
+};
+
+type RegionsDTO = {
+  regions: { [key: string]: string };
+};
+
+export const newsApi = createApi({
+  reducerPath: 'newsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: baseURL }),
+  endpoints: builder => ({
+    getNews: builder.query<newsDTO, string>({
+      query: name => ({
+        url: name,
+        params: { apiKey },
+        method: 'GET',
+      }),
+    }),
+    getCategories: builder.query<CategoriesDTO, string>({
+      query: name => ({
+        url: name,
+        method: 'GET',
+      }),
+    }),
+    getRegions: builder.query<RegionsDTO, string>({
+      query: name => ({
+        url: name,
+        method: 'GET',
+      }),
+    }),
+  }),
+});
+
+export const { useGetNewsQuery, useGetCategoriesQuery, useGetRegionsQuery } =
+  newsApi;
