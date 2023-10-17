5a8803c80586da962dbad63827574ea8cdc86584
BrunoUA96 <ostapklysa@gmail.com>
2023-10-17 14:18:05 +0100

    change name

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export * from './news-cards';
 export * from './categories.component';
 export * from './header.component';
 export * from './pagination.component';
-export * from './layout.component';
+export * from './llayout.component';
